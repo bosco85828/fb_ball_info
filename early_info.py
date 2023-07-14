2c20404254b811689022ff94cd8f8241427937af
bill jiang <bosco85828@gmail.com>
2023-07-14 14:18:43 +0800

    add_delsql

--- early_info.py
+++ early_info.py
@@ -2,6 +2,7 @@ import time
 from get_info import get_early_api 
 import schedule
 from datetime import datetime, timezone , timedelta
+from sqldb import del_info
 
 def job():
     with open('token.txt','r') as f :
@@ -10,8 +11,20 @@ def job():
     
     get_early_api(token)
 
+def del_job():
+
+    del_info('soccer')
+    del_info('basketball')
+    del_info('baseball')
+    del_info('tennis')
+    del_info('volleyball')
+    del_info('badminton')
+    del_info('football')
+    del_info('table_tennis')
+
 if __name__ == "__main__" : 
     schedule.every().day.at("00:00").do(job)
+    schedule.every().day.at("00:00").do(del_job)
 
     while True : 
         schedule.run_pending()
--- sqldb.py
+++ sqldb.py
@@ -89,6 +89,38 @@ def get_info(table_name,time_type):
     except : return None
 
 
+def del_info(table_name):
+    connection = pymysql.connect(
+        host=SQL_DOMAIN,
+        user='root',
+        password=SQL_PASSWORD,
+        database='ball_info',
+        charset='utf8mb4'
+    )
+
+
+    try : 
+        # 創建 cursor 對象
+        cursor = connection.cursor()
+        sql = f"delete from {table_name} where DATE(created_at) <= DATE_SUB(CURDATE(),INTERVAL 5 day)"
+        cursor.execute(sql)
+        connection.commit()
+        # print(results)
+        # print(results)
+        print(f"{table_name}資料刪除成功")
+            
+            
+    
+    except Exception as e :
+        print("刪除資料時發生錯誤：", str(e))
+
+    finally : 
+        cursor.close()
+        connection.close()
+    
+    
+
+
 if __name__ == "__main__":
     data={'start_at': '上半场01:05', 'home_name': 'FC东京', 'home_score': '0', 'home_win_odds': '3.21', 'home_handicap': '+0/0.5', 'home_handicap_odds': '1.85', 'home_total': '大 2/2.5', 'home_total_odds': '1.84', 'home_icon': 'https://static.fastbs66.com/data/6cc182ba1fc93b8feebe84e98196e846.png', 'guest_name': '名古屋鲸八', 'guest_score': '0', 'guest_win_odds': '2.23', 'guest_handicap': '-0/0.5', 'guest_handicap_odds': '1.95', 'guest_total': '小 2/2.5', 'guest_total_odds': '1.94', 'guest_icon': 'https://static.fastbs66.com/e6c1ffb40b97df90f60076d9c1ad0bae.png'}
     # print(json.dumps(data,ensure_ascii=False))
