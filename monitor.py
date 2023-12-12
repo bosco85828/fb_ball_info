a927cf40756d15122b047f22e9f70ea53fe0f229
bill jiang <bosco85828@gmail.com>
2023-12-12 14:01:14 +0800

    fix monitor

--- monitor.py
+++ monitor.py
@@ -2,7 +2,7 @@ import requests
 import time
 from dotenv import load_dotenv
 import os 
-
+from pprint import pprint
 load_dotenv()
 TG_TOKEN=os.getenv('TG_TOKEN')
 TG_CHAT=os.getenv('TG_CHAT')
@@ -22,25 +22,66 @@ def send_msg(msg):
     result=requests.post(url,json=data).json()
     return result
 
-def health_check(url):
-    data=requests.get(url).json()
-    return data['meta']
+
+def check_data(data):
+    badminton=data['data']['badminton']
+    baseball=data['data']['baseball']
+    basketball=data['data']['basketball']
+    football=data['data']['football']
+    soccer=data['data']['soccer']
+    table_tennis=data['data']['table_tennis']
+    tennis=data['data']['tennis']
+    volleyball=data['data']['volleyball']
+
+    del data
+    pprint(locals())
+    if not badminton and not baseball and not basketball and not football and not soccer and not table_tennis and not tennis and not volleyball : 
+        return False
+    
+    else : 
+        return True 
+    
+
 
 
 if __name__ == "__main__":
-    url="http://35.241.69.112:5000/GameMatchInfo/API.ASPX?ball=all&time=live&data=live"
     while True : 
-        meta=health_check(url)
-        if meta['status'] != "success" : 
-            print(meta['message'])
-            send_result=send_msg(f"FB sport: {meta['message']}")
-            
         try : 
-            print(send_result)
-            if not send_result['ok']:
-                print(send_result['description'])
-        except : 
-            pass
+            live_url="http://35.241.69.112:5000/GameMatchInfo/API.ASPX?ball=all&time=live"
+            today_url="http://35.241.69.112:5000/GameMatchInfo/API.ASPX?ball=all&time=today"
+            early_url="http://35.241.69.112:5000/GameMatchInfo/API.ASPX?ball=all&time=early"
+            url_list=[live_url,today_url,early_url]
+
+            score=0
+            for url in url_list : 
+                data=requests.get(url).json()
+                if data['meta']['status'] != "success" : 
+                    send_result=send_msg(f"FB sport:{url},{data['meta']['message']}")
+                if not check_data(data): 
+                    score+=1
+            else : 
+                if score >= 2 : 
+                    send_result=send_msg(f"FB sport: does not have data.")
+        except Exception as err :
+             send_result=send_msg(f"FB sport: {err}")
+            
+        finally : time.sleep(300)
+
+
+    # while True : 
+    #     meta=health_check(url)
+    #     if meta['status'] != "success" : 
+    #         print(meta['message'])
+    #         send_result=send_msg(f"FB sport: {meta['message']}")
+
+
         
-        time.sleep(120)
+    #     try : 
+    #         print(send_result)
+    #         if not send_result['ok']:
+    #             print(send_result['description'])
+    #     except : 
+    #         pass
+        
+    #     time.sleep(300)
             
